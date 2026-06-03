9b583e6f794fa147f8cbe2728a4b90bc1007b626
rinkal-01 <rinkal.magnatesage@gmail.com>
2026-06-03 12:33:44 +0530

    first commit

---
+++ program1.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+data = pd.read_csv('orders.csv')
+
+# aggregate functions: sum avg, mean 
+delivery = data["total_amount"].agg(["mean"])
+
+
+print(delivery)
+
+# mean: avg of the data
+# median: middle value
+# mode: kon si 
+
+
+
+# Appcount = 0
+# MobileWeb = 0
+# Website = 0
+
+# for i in data["channel"]:
+#     if i == "App":
+#         Appcount = Appcount+1
+#     if i=="Mobile Web":
+#         MobileWeb +=1
+#     if i=="Website":
+#         Website += 1
+
+
+
+# print(Appcount)
+# print(MobileWeb)
+# print(Website)
+
+# print(Appcount+MobileWeb+Website)
